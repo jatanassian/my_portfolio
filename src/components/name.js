4dbc446a76d7ed7049d72ba3befbd71884f616be
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-20 17:19:54 -0500

    create the component, which display my name and the github/linkedin links to my profile

---
+++ src/components/name.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Name = () => (
+    <div
+      style={{
+        margin: `0 auto`,
+        height: '100vh',
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+        border: 'solid 1px red'
+      }}
+    >
+      <h1 style={{ margin: 0, fontSize: '4rem' }}>
+        Julien Atanassian
+      </h1>
+      <h3>Full Stack Web Developer</h3>
+      <a href='https://github.com/jatanassian' rel="noopener noreferrer" target="_blank">GitHub</a>
+      <br />
+      <a href='https://www.linkedin.com/in/julien-atanassian/' rel="noopener noreferrer" target="_blank">LinkedIn</a>
+    </div>
+)
+
+export default Name
