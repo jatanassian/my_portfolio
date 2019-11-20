1c1c1152681beb22c31391078e1bd859dafaeb9b
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-20 18:26:04 -0500

    implement the about component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
 import React, { Fragment } from "react"
 
 import Name from "../components/name"
-import Image from "../components/image"
+import About from "../components/about"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Fragment>
     <SEO title="Home" />
     <Name/>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <About />
     <footer>
       © {new Date().getFullYear()}, Built with
       {` `}
