94a8f8a80abec70d6aa5a081af5268db318055ad
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-20 17:19:18 -0500

    remove the layout component and implement the name component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,24 @@
-import React from "react"
+import React, { Fragment } from "react"
 
-import Layout from "../components/layout"
+import Name from "../components/name"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <Fragment>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <Name/>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-  </Layout>
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+  </Fragment>
 )
 
 export default IndexPage
