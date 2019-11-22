83f1d9b665dec469223f168c06dc9b57f1ba6919
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-22 17:15:41 -0500

    add padding and margin to each card

--- src/components/project.js
+++ src/components/project.js
@@ -5,11 +5,11 @@ const Project = (props) => {
   const { name, description, tech, github, image } = props
   
   return (
-    <Card style={{ width: '25rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
+    <Card style={{ width: '22rem', margin: '0 1rem 2rem 1rem' }}>
+      <Card.Img style={{ marginBottom: '0' }}variant="top" src={image} />
+      <Card.Body style= {{ paddingTop: '.5rem' }}>
         <Card.Title>{name}</Card.Title>
-        <Card.Text>{description} <br />Made with {tech}</Card.Text>
+        <Card.Text style={{ fontSize: '.8rem' }}>{description} <br />Made using {tech}</Card.Text>
         <Button variant="outline-secondary"><a style={{ textDecoration: 'none' }} href={github} rel="noopener noreferrer" target="_blank">GitHub Repo</a></Button>
       </Card.Body>
     </Card>
