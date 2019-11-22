85fd34ca5b9afe148c1b54ac5e04f6f32eec4216
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-22 15:43:56 -0500

    add style

--- src/components/name.js
+++ src/components/name.js
@@ -5,17 +5,22 @@ const Name = () => (
       style={{
         margin: `0 auto`,
         height: '100vh',
-        padding: `1.45rem 1.0875rem`,
+        padding: `1.45rem 5rem`,
         border: 'solid 1px red',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
       }}
     >
       <h1 style={{ margin: 0, fontSize: '4rem', color: '#000080' }}>
         <strong>Julien Atanassian</strong>
       </h1>
-      <h3 style={{ color: '#000080' }}>Full Stack Web Developer</h3>
-      <a href='https://github.com/jatanassian' rel="noopener noreferrer" target="_blank"><img src={require('../images/github-logo.svg')} alt='GitHub' style={{ width: '1.8rem', height: 'auto', marginRight: '.8rem' }}/></a>
-      <a href='https://www.linkedin.com/in/julien-atanassian/' rel="noopener noreferrer" target="_blank"><img src={require('../images/linkedin-logo.svg')} alt='LinkedIn' style={{ width: '1.8rem', height: 'auto', marginRight: '.8rem' }}/></a>
-      <a href="mailto:julien.atanassian@gmail.com"><img src={require('../images/email-logo.svg')} alt='julien.atanassian@gmail.com' style={{ width: '1.8rem', height: 'auto', marginTop: '8px' }}/></a>
+      <h3 style={{ color: '#000080', fontSize: '1.2rem' }}>Full Stack Web Developer</h3>
+      <div>
+        <a href='https://github.com/jatanassian' rel="noopener noreferrer" target="_blank"><img src={require('../images/github-logo.svg')} alt='GitHub' style={{ width: '1.5rem', height: 'auto', marginRight: '.8rem' }}/></a>
+        <a href='https://www.linkedin.com/in/julien-atanassian/' rel="noopener noreferrer" target="_blank"><img src={require('../images/linkedin-logo.svg')} alt='LinkedIn' style={{ width: '1.5rem', height: 'auto', marginRight: '.8rem' }}/></a>
+        <a href="mailto:julien.atanassian@gmail.com"><img src={require('../images/email-logo.svg')} alt='julien.atanassian@gmail.com' style={{ width: '1.5rem', height: 'auto', marginTop: '8px' }}/></a>
+      </div>
     </div>
 )
 
