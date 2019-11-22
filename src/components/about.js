59f3d5301e3d1a8bb4ca747c478dbe9f52d49c42
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-22 11:12:30 -0500

    remove bullets

--- src/components/about.js
+++ src/components/about.js
@@ -10,39 +10,35 @@ const About = () => (
     >
       <h3>About me</h3>
       <p>Hi, I'm a full stack web developer with a background in marketing based in Ottawa.</p>
-      <ul>
-        <li>Languages:</li>
-        <li>HTML5 & CSS3</li>
-        <li>JavaScript</li>
-        <li>Ruby</li>
-      </ul>
-      <ul>
-        <li>Frameworks & Librairies:</li>
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>React.js</li>
-        <li>jQuery</li>
-        <li>Ruby on Rails</li>
-      </ul>
-      <ul>
-        <li>Databases & Systems:</li>
-        <li>SQL</li>
-        <li>PostgreSQL</li>
-        <li>Git</li>
-      </ul>
-      <ul>
-        <li>Languages:</li>
-        <li>HTML5 & CSS3</li>
-        <li>JavaScript</li>
-        <li>Ruby</li>
-      </ul>
-      <ul>
-        <li>Testing frameworks:</li>
-        <li>Mocha & Chai</li>
-        <li>Jest</li>
-        <li>Cypress</li>
-        <li>Capybara</li>
-      </ul>
+      <div style={{display: 'flex'}}>
+        <ul style={{listStyle: 'none'}}>
+          <li>Languages:</li>
+          <li>HTML5 & CSS3</li>
+          <li>JavaScript</li>
+          <li>Ruby</li>
+        </ul>
+        <ul style={{listStyle: 'none'}}>
+          <li>Frameworks & Librairies:</li>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>React.js</li>
+          <li>jQuery</li>
+          <li>Ruby on Rails</li>
+        </ul>
+        <ul style={{listStyle: 'none'}}>
+          <li>Databases & Systems:</li>
+          <li>SQL</li>
+          <li>PostgreSQL</li>
+          <li>Git</li>
+        </ul>
+        <ul style={{listStyle: 'none'}}>
+          <li>Testing frameworks:</li>
+          <li>Mocha & Chai</li>
+          <li>Jest</li>
+          <li>Cypress</li>
+          <li>Capybara</li>
+        </ul>
+      </div>
     </div>
 )
 
