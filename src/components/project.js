a1d644bc123a2a8a8fbbc58301ec63ae1a88359e
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-22 17:40:07 -0500

    change style of the github link

--- src/components/project.js
+++ src/components/project.js
@@ -5,12 +5,14 @@ const Project = (props) => {
   const { name, description, tech, github, image } = props
   
   return (
-    <Card style={{ width: '22rem', margin: '0 1rem 2rem 1rem' }}>
+    <Card style={{ width: '22rem', margin: '0 1rem 2rem 1rem', fontSize: '.8rem' }}>
       <Card.Img style={{ marginBottom: '0' }}variant="top" src={image} />
-      <Card.Body style= {{ paddingTop: '.5rem' }}>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text style={{ fontSize: '.8rem' }}>{description} <br />Made using {tech}</Card.Text>
-        <Button variant="outline-secondary"><a style={{ textDecoration: 'none' }} href={github} rel="noopener noreferrer" target="_blank">GitHub Repo</a></Button>
+      <Card.Body style= {{ padding: '.5rem 1rem' }}>
+        <Card.Title style={{fontSize: '1rem', marginBottom: '.4rem' }}>{name}</Card.Title>
+        <Card.Text style = {{ marginBottom: '.5rem' }}>{description} <br />Made using {tech}</Card.Text>
+        <a style={{ color: 'inherit', textDecoration: 'none', fontSize: '.8rem' }} href={github} rel="noopener noreferrer" target="_blank">
+          <img src={require('../images/github-logo.svg')} alt='GitHub' style={{ width: '1rem', height: 'auto', margin: '0 .2rem .3rem 0' }}/> <u>GitHub Repo</u>
+        </a>
       </Card.Body>
     </Card>
   )
