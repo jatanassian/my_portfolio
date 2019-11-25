453ebf5de00a2af447db5808e5b2ffe46de50e72
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-25 11:46:31 -0500

    reduce font size of description

--- src/components/project.js
+++ src/components/project.js
@@ -9,7 +9,7 @@ const Project = (props) => {
       <Card.Img style={{ marginBottom: '0' }}variant="top" src={image} />
       <Card.Body style= {{ padding: '.5rem 1rem' }}>
         <Card.Title style={{fontSize: '1rem', marginBottom: '.4rem' }}>{name}</Card.Title>
-        <Card.Text style = {{ marginBottom: '.5rem' }}>{description} <br />Made using {tech}</Card.Text>
+        <Card.Text style = {{ marginBottom: '.5rem', fontSize: '.75rem' }}>{description} <br />Made using {tech}</Card.Text>
         <a style={{ color: 'inherit', textDecoration: 'none', fontSize: '.8rem' }} href={github} rel="noopener noreferrer" target="_blank">
           <img src={require('../images/github-logo2.png')} alt='GitHub' style={{ width: '1rem', height: 'auto', margin: '0 .2rem .3rem 0' }}/> <u>GitHub Repo</u>
         </a>
