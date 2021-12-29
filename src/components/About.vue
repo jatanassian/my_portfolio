76222943afc5709a7d06fa2ba443184e71b33b61
Julien Atanassian <julien.atanassian@gmail.com>
2021-12-28 21:02:13 -0500

    add text

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <h3>About me</h3>
+  <section class="about">
+    <h2>About</h2>
+    
+    <div>Hi, I'm a full stack web developer with a background in marketing based in Ottawa.</div>
+
+    <div>I enjoy the never ending learning journey that is web development. I also like solving problems and being part of a community. Member of Ottawa JavaScript, Ottawa Ruby & React Ottawa.</div>
+
+    <div>If you want to contact me, send me an email at julien.atanassian@gmail.com</div>
+  </section>
 </template>
 
 <script>
@@ -8,6 +16,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.about {
+  margin: 0 5rem;
+  padding: 3rem 0;
+  border-top: solid 1px #ffffff;
+  border-bottom: solid 1px #ffffff;
+}
 
+h2 {
+  font-size: 40px;
+}
 </style>
