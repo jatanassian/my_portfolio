0ea8a0b2c620ae4b0be1ca21a4519147c599f4a4
Julien Atanassian <julien.atanassian@gmail.com>
2021-12-28 19:36:45 -0500

    add hero section and navbar

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="text">
+    <h1>Julien Atanassian</h1>
+    <h2>Full Stack Web Developer</h2>
+    <div class="socials">
+      <img src="@/assets/github.png" alt="GitHub">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroSection',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text {
+  border: solid 1px red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 500px;
+  color: #f8f8ff;
+}
+
+h1 {
+  font-size: 70px;
+}
+
+h2 {
+  font-size: 40px;
+}
+
+.socials {
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  height: 2rem;
+}
+
+img:hover {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="navbar">
+    <span class="item">About</span>
+    <span class="item">Projects</span>
+    <button class="item">Contact</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+
+}
+</script>
+
+<style scoped>
+.navbar {
+  /* border: solid 1px green;
+  position: fixed; */
+  padding: .5rem;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.item {
+  margin: 0 1rem;
+}
+
+button {
+  background-color: #42b983;
+  color: white;
+  border: none;
+  padding: .7rem 1.3rem;
+  border-radius: 30px;
+  font-weight: bold;
+}
+
+button:hover {
+  cursor: pointer;
+}
+</style>
