02422d5e12bffb32cc2a02a0e60b4059c0b69a56
Julien Atanassian <julien.atanassian@gmail.com>
2022-01-28 16:34:54 -0500

    add screenshot, footer and description of project

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -4,7 +4,17 @@
       <span class="circle red"></span>
       <span class="circle orange"></span>
       <span class="circle green"></span>
-      <span>CSS Playground</span>
+      <h3>CSS Playground</h3>
+    </div>
+    <img src="@/assets/images/css-playground.png" alt="css-playground-screenshot">
+    <div class="content">A webapp that let's you play with different CSS properties (rotate, skew...) and copy the code. <br> Made using Vue.js</div>
+    <div class="footer">
+      <a href="https://github.com/jatanassian" target="_blank" rel="noreferrer noopener">
+        <img src="@/assets/icons/code.png" alt="See code" />
+      </a>
+      <a href="https://github.com/jatanassian" target="_blank" rel="noreferrer noopener">
+        <img src="@/assets/icons/live.png" alt="See app" />
+      </a>
     </div>
   </div>
 </template>
@@ -18,14 +28,13 @@ export default {
 <style scoped>
 .project {
   width: 300px;
-  height: 450px;
   background-color: rgba(255, 255, 255, .15);  
   backdrop-filter: blur(5px);
-  border-radius: 10px;
+  border-radius: 15px;
 }
 
 .header {
-  padding: 5px;
+  padding: 5px 9px;
   display: flex;
   align-items: center;
   font-weight: bold;
@@ -49,5 +58,31 @@ export default {
 
 .green {
   background-color: rgb(0, 202, 78);
+  margin-right: 12px;
+}
+
+img {
+  width: 100%;
+}
+
+.content {
+  margin: 10px;
+  font-size: .9rem;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px;
+}
+
+.footer img {
+  height: 2rem;
+  width: auto;
+}
+
+.footer img:hover {
+  cursor: pointer;
 }
 </style>
