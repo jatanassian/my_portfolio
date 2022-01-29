a8ffec0f3dbbc9030632a8a06166bf8000a78d2c
Julien Atanassian <julien.atanassian@gmail.com>
2022-01-29 01:17:36 -0500

    reverse projects order

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,7 +2,7 @@
   <section class="projects">
     <h2>Projects</h2>
     <div class="projects-container">
-      <Project v-for="project in projects" :key="project.key" :project="project"/>
+      <Project v-for="project in projects.slice().reverse()" :key="project.key" :project="project"/>
     </div>
   </section>
 </template>
@@ -19,24 +19,42 @@ export default {
       projects: [
         {
           key: 1,
-          name: "CSS Playground",
-          description: "A webapp that let's you play with different CSS properties (rotate, skew...) and copy the code.",
-          tech: "Vue.js",
-          github: "https://github.com/jatanassian/css-playground",
-          url: "https://vigorous-lichterman-d7a26f.netlify.app",
-          image: "css-playground"
+          name: "TinyApp",
+          description: "A URL shortening app with analytics.",
+          tech: "Node.js, Express, EJS, bcrypt, cookie-session, Mocha, Chai",
+          github: "https://github.com/jatanassian/tinyapp",
+          url: null,
+          image: "tinyapp"
         },
         {
           key: 2,
-          name: "Nomad",
-          description: "A travel planner which suggests cities, activities and estimated cost. Once a city is added to the trip it suggests you a new city to explore around the previous one.",
-          tech: "React.js, Node.js, PostgreSQL, knex.js, Google Maps API, Nodemailer, SASS/SCSS, Bootstrap",
-          github: "https://github.com/jatanassian/Nomad",
+          name: "Tweeter",
+          description: "A responsive single-page Twitter clone.",
+          tech: "jQuery, CSS, AJAX, Node.js",
+          github: "https://github.com/jatanassian/tweeter",
           url: null,
-          image: "nomad"
+          image: "tweeter"
         },
         {
           key: 3,
+          name: "Memo",
+          description: "A web application inspired by Reddit allowing users to post content, like, rate and comment posts, create a collection of posts and review all their activity.",
+          tech: "EJS, jQuery, Node.js, AJAX, SASS/SCSS, PostgreSQL",
+          github: "https://github.com/jatanassian/Memo",
+          url: null,
+          image: "memo"
+        },
+        {
+          key: 4,
+          name: "Interview scheduler",
+          description: "Single-page app that allows users to book, edit or cancel interviews for each day of the week.",
+          tech: "React.js, Node.js, Jest, Cypress",
+          github: "https://github.com/jatanassian/scheduler",
+          url: "https://competent-kepler-8ba31c.netlify.com",
+          image: "scheduler"
+        },
+        {
+          key: 5,
           name: "Jungle",
           description: "A mini e-commerce application which allows users to view and purchase products with Stripe verification. Admins can add products and categories.",
           tech: "Rails 4.2, PostgreSQL, SASS/SCSS, Bootstrap 4",
@@ -45,40 +63,22 @@ export default {
           image: "jungle"
         },
         {
-          key: 4,
-          name: "Interview scheduler",
-          description: "This is a single-page app built on React.js that allows users to book, edit or cancel interviews for each day of the week .",
-          tech: "React.js, Node.js, Jest, Cypress",
-          github: "https://github.com/jatanassian/scheduler",
-          url: "https://competent-kepler-8ba31c.netlify.com",
-          image: "scheduler"
-        },
-        {
-          key: 5,
-          name: "Memo",
-          description: "A web application inspired by Reddit that allows users to post content, like, rate and comment posts, create a collection of posts and review all their activity.",
-          tech: "EJS, jQuery, Node.js, AJAX, SASS/SCSS, PostgreSQL",
-          github: "https://github.com/jatanassian/Memo",
-          url: null,
-          image: "memo"
-        },
-        {
           key: 6,
-          name: "Tweeter",
-          description: "A simple and responsive single-page Twitter clone.",
-          tech: "jQuery, CSS, AJAX, Node.js",
-          github: "https://github.com/jatanassian/tweeter",
+          name: "Nomad",
+          description: "A travel planner which suggests cities, activities and estimated cost. Once a city is added to the trip it suggests you a new city to explore around the previous one.",
+          tech: "React.js, Node.js, PostgreSQL, knex.js, Google Maps API, Nodemailer, SASS/SCSS, Bootstrap",
+          github: "https://github.com/jatanassian/Nomad",
           url: null,
-          image: "tweeter"
+          image: "nomad"
         },
         {
           key: 7,
-          name: "TinyApp",
-          description: "A URL shortening app with basic analytics.",
-          tech: "Node.js, Express, EJS, bcrypt, cookie-session, Mocha, Chai",
-          github: "https://github.com/jatanassian/tinyapp",
-          url: null,
-          image: "tinyapp"
+          name: "CSS Playground",
+          description: "A webapp that let's you play with different CSS properties (rotate, skew...) and copy the code.",
+          tech: "Vue.js",
+          github: "https://github.com/jatanassian/css-playground",
+          url: "https://vigorous-lichterman-d7a26f.netlify.app",
+          image: "css-playground"
         },
       ]
     }
@@ -98,6 +98,7 @@ export default {
   display: grid;
   grid-template-columns: repeat(3, minmax(200px, 1fr));
   justify-items: center;
+  align-items: start;
   gap: 1rem;
 }
 
