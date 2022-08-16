2fdcd1b0b0a1371a77ad8cdbf313e024cc0c197a
Julien Atanassian <julien.atanassian@gmail.com>
2022-08-16 16:58:21 -0400

    add resume link

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,14 +3,37 @@
     <h1>Julien Atanassian</h1>
     <h2>Full Stack Web Developer</h2>
     <div class="socials">
-      <a href="https://github.com/jatanassian" target="_blank" rel="noreferrer noopener" class="grow">
+      <a
+        href="https://github.com/jatanassian"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="grow"
+      >
         <img src="@/assets/icons/github.png" alt="GitHub" />
       </a>
-      <a href="https://www.linkedin.com/in/julien-atanassian/" target="_blank" rel="noreferrer noopener" class="grow">
+      <a
+        href="https://www.linkedin.com/in/julien-atanassian/"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="grow"
+      >
         <img src="@/assets/icons/linkedin-logo.png" alt="LinkedIn" />
       </a>
-      <a href="https://twitter.com/jatanassian" target="_blank" rel="noreferrer noopener" class="grow">
+      <a
+        href="https://twitter.com/jatanassian"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="grow"
+      >
         <img src="@/assets/icons/twitter.png" alt="Twitter" />
+      </a>
+      <a
+        href="https://resume.creddle.io/resume/emm9svs875o"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="grow"
+      >
+        <img src="@/assets/icons/cv.png" alt="Resume" />
       </a>
     </div>
     <ContactButton class="contact-btn" />
@@ -18,12 +41,12 @@
 </template>
 
 <script>
-import ContactButton from './ui/ContactButton.vue';
+import ContactButton from "./ui/ContactButton.vue";
 
 export default {
-  name: 'HeroSection',
-  components: { ContactButton }
-}
+  name: "HeroSection",
+  components: { ContactButton },
+};
 </script>
 
 <style scoped>
@@ -66,15 +89,25 @@ img:hover {
 }
 
 @media (min-width: 400px) {
-  h1 { font-size: 50px;}
-  h2 { font-size: 25px;}
+  h1 {
+    font-size: 50px;
+  }
+  h2 {
+    font-size: 25px;
+  }
 }
 @media (min-width: 501px) {
-  .contact-btn { display: none; }
+  .contact-btn {
+    display: none;
+  }
 }
 
 @media (min-width: 750px) {
-  h1 { font-size: 70px;}
-  h2 { font-size: 40px;}
+  h1 {
+    font-size: 70px;
+  }
+  h2 {
+    font-size: 40px;
+  }
 }
 </style>
